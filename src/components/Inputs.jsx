41e698aa428418c04071ed55b6b4c447bf34ebf2
FAVOURS EXCELLENCE <125229328+excellar-BX@users.noreply.github.com>
2025-03-29 08:00:23 +0100

    Update Inputs.jsx

--- src/components/Inputs.jsx
+++ src/components/Inputs.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
-const Inputs = ({label, type, placeholder, value}) => {
+const Inputs = ({label, type, placeholder, onChange}) => {
   return (
     <div className='flex flex-col sm:max-w-[500px] sm:mx-auto my-6 max-w-[300px] '  >
     <label className=' text-xl ml-4 font-bold ' htmlFor={label}>{label} {"*"}</label>
-    <input className=' w-[100%] mx-auto h-12 pl-4 bg-opacity-5 bg-blue-stone-500 outline-none border-none p-2  rounded-full ' type={type} value={value} placeholder={placeholder} />
+    <input className=' w-[100%] mx-auto h-12 pl-4 bg-opacity-5 bg-blue-stone-500 outline-none border-none p-2  rounded-full ' type={type} onChange={onChange} placeholder={placeholder} />
     </div>
   )
 }
